11307105e68f68ff1597a633307e8fb6dd267571
samir-sah <sahsamir2004@gmail.com>
2026-05-02 23:41:36 +0530

    initial fullstack setup (next frontend + node backend)

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv"
+dotenv.config({
+    path:"./.env"
+});
+
+import app from "./app.js";
+import connectDB from "./db/database.js";
+
+const port = process.env.PORT || 3000;
+
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`example app listening on port http://localhost:${port}`);
+        });
+    })
+    .catch((err) =>
+    {
+        console.error("mongodb connection error",err)
+        process.exit(1);
+    })
+
