7bfe287309a89af1d5152689f5686a8d9715965e
samir-sah <sahsamir2004@gmail.com>
2026-05-05 15:48:34 +0530

    font selection

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,10 +1,18 @@
 import "./globals.css";
 import Sidebar from "../components/sidebar";
 import Header from "../components/header";
+import { Montserrat } from "next/font/google";
+
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={montserrat.className}>
         <Sidebar />
         <Header />
 
--- client/components/sidebar.js
+++ client/components/sidebar.js
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 export default function Sidebar(){
     return (
-        <div className="h-[calc(100vh-4rem)] w-50 bg-gray-900 text-white fixed left-0 top-16 p-5">
+        <div className="h-[calc(100vh-4rem)] w-56 bg-white text-blue-500 fixed left-0 top-16 p-5 font-semibold ">
             <nav className="flex flex-col gap-4">
-                <Link href="/"className="hover: text-gray-300">Dashboard</Link>
-                <Link href="/dashboard"className="hover: text-gray-300">Customer</Link>
-                <Link href="/orders"className="hover: text-gray-300">Orders</Link>
-                <Link href="/settings"className="hover: text-gray-300">Revenue</Link>
+                <Link href="/"className="hover:text-indigo-900 font-bold">Dashboard</Link>
+                <Link href="/dashboard"className="hover:text-gray-300">Customer</Link>
+                <Link href="/orders"className="hover:text-gray-300">Orders</Link>
+                <Link href="/settings"className="hover:text-gray-300">Revenue</Link>
 
             </nav>
         </div>
