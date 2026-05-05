997d4d6c2253140fd8d570b584ee620723494d05
samir-sah <sahsamir2004@gmail.com>
2026-05-05 15:20:51 +0530

    adjusted sidebar dimensions

--- client/components/sidebar.js
+++ client/components/sidebar.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Sidebar(){
     return (
-        <div className="h-[calc(100vh-4rem)] w-64 bg-gray-900 text-white fixed left-0 top-16 p-5">
+        <div className="h-[calc(100vh-4rem)] w-50 bg-gray-900 text-white fixed left-0 top-16 p-5">
             <nav className="flex flex-col gap-4">
                 <Link href="/"className="hover: text-gray-300">Dashboard</Link>
                 <Link href="/dashboard"className="hover: text-gray-300">Customer</Link>
